0cd9dc4ac7729839acdb49f99f70f9a815f4476d
shenhb <shenhb@topband.com.cn>
2018-10-10 17:56:07 +0800

    updating preload.sh

--- preload/preload.sh
+++ preload/preload.sh
@@ -5,8 +5,12 @@ SDCARD_PATH=/mnt/internal_sd
 PRELOAD_DEST=${SDCARD_PATH}/preload
 PRELOAD_FLAG=${PRELOAD_DEST}/.preload
 
-OLD_VERSION=`grep "ro.topband.sw.versioncode" ${PRELOAD_FLAG}`
-CUR_VERSION=`grep "ro.topband.sw.versioncode" /system/build.prop`
+while [ ! -d ${SDCARD_PATH}/Android ] ; do
+	echo "preload: ${SDCARD_PATH}/Android no exist and wait 3s."
+	sleep 3
+done
+
+rm -rf ${SDCARD_PATH}/LOST.DIR/*
 
 if [ ! -d ${PRELOAD_SOURCE} ]
 then
@@ -14,17 +18,15 @@ then
     exit 0
 fi
 
-while [ ! -d ${SDCARD_PATH}/Android ] ; do
-	echo "preload: ${SDCARD_PATH}/Android no exist and wait 3s."
-	sleep 3
-done
-
 if [ ! -d ${PRELOAD_DEST} ]
 then
     mkdir -p ${PRELOAD_DEST}
 fi
 
+OLD_VERSION=`grep "ro.topband.sw.versioncode" ${PRELOAD_FLAG}`
+CUR_VERSION=`grep "ro.topband.sw.versioncode" /system/build.prop`
 echo "preload: OLD_VERSION=${OLD_VERSION}, CUR_VERSION=${CUR_VERSION}"
+
 if [ "${CUR_VERSION}" != "${OLD_VERSION}" ]
 then
 	rm -rf ${PRELOAD_DEST}/*
